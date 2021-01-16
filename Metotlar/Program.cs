88727c67c3e0b1a7c793570008aba1616d0614b5
İsmail Ekinci <Smaleknc@outlook.com>
2021-01-16 23:42:10 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya ELması";
+
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karbuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karbuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+            foreach (var urun in urunler)
+            {
+
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-------------------");
+
+
+            }
+
+
+            Console.WriteLine("--------Metorlar----------");
+
+            SepetManager sepetManager = new SepetManager();
+
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+            sepetManager.Ekle2("Karpuz" , "Diyarbakır Karpuzu",80,12);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+//Dont repeat yourself-Clean Code-Best Practice
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming convention
+        //syntax
+        public void Ekle(Urun urun)
+        {
+
+
+            Console.WriteLine("Tebrikler Sepete Eklendi : " + urun.Adi + " : " + urun.Fiyati);
+
+        }
+
+        public void Ekle2(string urunAdi,string urunAciklamasi,int urunFiyati,int urunStokAdedi)
+        {
+            Console.WriteLine("Tebrikler Sepete Eklendi : " + urunAdi + " : " + urunAciklamasi + " : " + urunFiyati + " : " + urunStokAdedi);
+        }
+    }
+}
